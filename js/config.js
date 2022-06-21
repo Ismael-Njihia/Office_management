fd42e71caa30b385d6b9f0343187214b1a827fc0
EngIshmael <engIshmaelnjihia@gmail.com>
2022-06-21 12:05:28 +0300

    Login specification for Hr, Admin and finance

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApSsLCZynde9SmZ75ZsdOFVhj5I-mD1iE",
+    authDomain: "ofisi-4b35e.firebaseapp.com",
+    projectId: "ofisi-4b35e",
+    storageBucket: "ofisi-4b35e.appspot.com",
+    messagingSenderId: "360987545116",
+    appId: "1:360987545116:web:7c2216b19eb92ab51559d9",
+    measurementId: "G-4CSY7QY9VT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ js/login.js
@@ -0,0 +1,27 @@
+document.getElementById("login").onclick = function() {
+
+    let Email = document.getElementById("Email").value;
+    let Password = document.getElementById("Password").value;
+    //summon firebase to authorise a login
+    firebase.auth().signInWithEmailAndPassword(Email, Password).then((userCred) => {
+        let userId = userCred.user.uid;
+        //getting the UserType from the firestore
+        firebase.firestore().collection("Users").doc(userId).get().then((user) => {
+            let UserType = user.data().userType;
+
+            if (UserType == "admin") {
+                window.location.href = "dashboard.html";
+            } else if (UserType == "Hr") {
+                window.location.href = "../Office_management/dashboard/Hr.html";
+            } else if (UserType == "finance") {
+                window.location.href = "../Office_management/dashboard/finance.html";
+            } else {
+                alert("UserType not found");
+            }
+        })
+
+    }).catch((error) => {
+        const error1 = error.message;
+        alert(error1);
+    });
+}
