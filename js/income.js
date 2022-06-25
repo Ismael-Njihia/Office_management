c0c9451feb57f5787c004c3674df3b78e531bf18
EngIshmael <engIshmaelnjihia@gmail.com>
2022-06-25 18:29:26 +0300

    Income UI

--- js/income.js
+++ js/income.js
@@ -12,39 +12,61 @@ firebase.auth().onAuthStateChanged((user) => {
                 document.getElementById("navUserType").innerText = userType;
             })
             //show the paymentMethod from the settings
-        firebase.firestore().collection("paymentMethod").get().then((querySnapshot) => {
-            let content = '';
-            querySnapshot.forEach((doc) => {
-                let pMethod = doc.data().pMethodInput;
-                let docId = doc.data().docId;
+        firebase.firestore().collection("dPaymentMethod").get().then((querySnapshot) => {
+                let content = '';
+                querySnapshot.forEach((doc) => {
+                    let pMethod = doc.data().pMethodInput;
+                    let docId = doc.data().docId;
 
-                content += '<option value="' + pMethod + '">' + pMethod + '</option';
+                    content += '<option value="' + pMethod + '">' + pMethod + '</option>';
+
+                })
+                $("#paymentMethod").append(content);
+            })
+            //user adding the Income
+        document.getElementById("saveIncome").onclick = function() {
+
+                let incAmount = document.getElementById("incAmount").value;
+                let incomeAddTime = document.getElementById("incomeAddTime").value;
+                let incomeFrom = document.getElementById("incomeFrom").value;
+                let incomeDescription = document.getElementById("incomeDescription").value;
+                let paymentMethod = document.getElementById("paymentMethod").value;
+
+                let incomeDoc = firebase.firestore().collection("income").doc();
+                incomeDoc.set({
+                    incAmount: incAmount,
+                    incomeAddTime: incomeAddTime,
+                    incomeFrom: incomeFrom,
+                    incomeDescription: incomeDescription,
+                    paymentMethod: paymentMethod,
+                    incomeDoc: incomeDoc.id
+                }).then(() => {
+                    window.location.reload();
+                })
+            }
+            //showing to the User!
+        firebase.firestore().collection("income").get().then((querySnapshot) => {
+            let contentIncome = '';
+            querySnapshot.forEach((doc) => {
+                let incAmount = doc.data().incAmount;
+                let incomeAddTime = doc.data().incomeAddTime;
+                let incomeFrom = doc.data().incomeFrom;
+                let paymentMethod = doc.data().paymentMethod;
+                let incomeDescription = doc.data().incomeDescription;
+
+                contentIncome += '<tr>';
+                contentIncome += '<td>' + incAmount + '</td>';
+                contentIncome += '<td>' + incomeAddTime + '</td>';
+                contentIncome += '<td>' + incomeFrom + '</td>';
+                contentIncome += '<td>' + paymentMethod + '</td>';
+                contentIncome += '<td>' + incomeDescription + '</td>';
+                contentIncome += '</tr>';
 
             })
-            $("#paymentMethod").append(content);
+            $("incomeDisplay").append(contentIncome);
         })
 
-        //capture the user input
-        document.getElementById("submit").onclick = function() {
-            let incomeAmount = document.getElementById("incAmount").Value;
-            let incomeAddTime = document.getElementById("incomeAddTime").Value;
-            let incomeFrom = document.getElementById("incomeFrom").Value;
-            let incomeDescription = document.getElementById("incomeDescription").Value;
-            // let paymentMethod = document.getElementById("paymentMethod").Value;
-            console.log(incomeAddTime);
 
-            let addIncome = firebase.firestore().collection("income").doc();
-            addIncome.set({
-                docId: addIncome.id,
-                incomeAmount: incomeAmount,
-                incomeAddTime: incomeAddTime,
-                incomeFrom: incomeFrom,
-                incomeDescription: incomeDescription,
-                paymentMethod: paymentMethod
-            }).then(() => {
-                window.location.reload;
-            })
-        }
     } else {
         window.location.href = "/Office_management/index.html"
     }
